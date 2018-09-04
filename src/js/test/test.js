14b94a03972ef1a7f89360c73ef402ecd7f9653c
Elwerco <alexsteev2002@gmail.com>
2018-09-05 00:55:41 +0300

    final 3:

--- src/js/test/test.js
+++ src/js/test/test.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Table from '../components/Table.jsx'
 import Adapter from 'enzyme-adapter-react-16'
 
-it('renders an editor area', () => {
+import Table from '../components/Table.jsx'
+import Tr from '../components/Tr.jsx'
+import Td from '../components/Td.jsx'
+
+it('Table test', () => {
   const editor = shallow(<Table />)
-  expect(editor.find('table')).toBeTruthy()﻿
+  expect(editor.find('.table')).toHaveLength(1)
+})
+
+it('Tr test', () => {
+  const editor = shallow(<Tr />)
+  expect(editor.find('.tr')).toHaveLength(1)
+})
+
+// Так как в момент теста данные не получены и ячеики таблицы не созданы(на кнопку никто не тыкнул),
+// я проверяю наичие пустого компонента, и в слуае если такой существует-возвращаю true.
+
+it('Td test', () => {
+  const editor = shallow(<Td />)
+  expect(editor.exists()).toEqual(true)
 })
